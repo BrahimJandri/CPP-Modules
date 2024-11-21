aa74a1379b767a33fa2a65672fdca848753b5b16
BrahimJandri <bzaza066@gmail.com>
2024-11-21 20:29:26 +0100

    work on ex02

--- cpp00/ex02/Account.cpp
+++ cpp00/ex02/Account.cpp
@@ -2,9 +2,23 @@
 #include <ctime>
 
 
-s
-
-static int Account::getNbAccounts( void )
+int Account::getNbAccounts( void )
 {
     return _nbAccounts;
 }
+
+int Account::getNbDeposits( void )
+{
+    return _totalNbDeposits;
+}
+
+int Account::getTotalAmount( void )
+{
+    return _totalAmount;
+}
+
+int Account::getNbWithdrawals( void )
+{
+    return _totalNbWithdrawals;
+}
+
