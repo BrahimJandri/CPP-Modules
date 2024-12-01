975d8222a16bc154aed5c9502d66f4178663af2d
BrahimJandri <bzaza066@gmail.com>
2024-12-01 13:18:29 +0100

    start ex03

---
+++ cpp01/ex03/HumanA.hpp
@@ -0,0 +1,12 @@
+#ifndef HUMANA_HPP
+#define HUMANA_HPP
+
+#include <iostream>
+
+class HumanA
+{
+private:
+    std::string type;
+};
+
+#endif
---
+++ cpp01/ex03/HumanB.hpp
@@ -0,0 +1,12 @@
+#ifndef HUMANB_HPP
+#define HUMANB_HPP
+
+#include <iostream>
+
+class HumanB
+{
+private:
+    std::string type;
+};
+
+#endif
