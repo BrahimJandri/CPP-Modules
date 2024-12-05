22606a95cef7be6a7b0ac8729f3ece39e194b2bf
BrahimJandri <bzaza066@gmail.com>
2024-12-05 10:53:03 +0100

    start cpp02

--- cpp01/ex01/main.cpp
+++ cpp01/ex01/main.cpp
@@ -2,13 +2,12 @@
 
 int main()
 {
-    Zombie *horde = zombieHorde(1000000000, "Zombie");
-    if(!horde)
+    Zombie *horde = zombieHorde(1, "Zombie");
+    if (!horde)
     {
-        std::cout << "7ze9 lprogramm" << std::endl;
         return 1;
     }
-    for (int i = 0; i <1000000000 ; i++)
+    for (int i = 0; i < 1000000000; i++)
     {
         horde[i].announce();
     }
--- cpp01/ex01/zombieHorde.cpp
+++ cpp01/ex01/zombieHorde.cpp
@@ -5,7 +5,6 @@ Zombie *zombieHorde(int N, std::string name)
     Zombie *horde = new (std::nothrow) Zombie[N];
     if (!horde)
     {
-        std::cerr << "Can't allocate memory for Zombie\n";
         return NULL;
     }
     for (int i = 0; i < N; i++)
