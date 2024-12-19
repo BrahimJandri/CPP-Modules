0ffc092224264b9f3588c6d5c81e7aeabaf2c541
BrahimJandri <bzaza066@gmail.com>
2024-12-19 09:04:33 +0100

    ex00 complet

--- cpp02/ex00/Fixed.hpp
+++ cpp02/ex00/Fixed.hpp
@@ -1,13 +1,21 @@
-#pragma ONCE
+#ifndef FIXED_HPP
+#define FIXED_HPP
+
+#include <iostream>
 
 class Fixed
 {
-    int fixed_point;
-    static const int fractional;
+private:
+    int fixedPointValue;
+    static const int fractionalBits = 8;
 
 public:
     Fixed();
-    Fixed(const Fixed &fixed);
-    Fixed &operator=(const Fixed &fixed);
+    Fixed(const Fixed &other);
+    Fixed &operator=(const Fixed &other);
     ~Fixed();
+    int getRawBits() const;
+    void setRawBits(int const raw);
 };
+
+#endif
