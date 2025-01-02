b57bdb525f464709ea08fd809756dcb53bc94ec6
BrahimJandri <bzaza066@gmail.com>
2025-01-02 10:11:17 +0100

    start cpp03

---
+++ cpp03/ex00/Claptrap.hpp
@@ -0,0 +1,17 @@
+#ifndef CLAPTRAP_HPP
+#define CLAPTRAP_HPP
+
+#include <iostream>
+
+class ClapTrap
+{
+private:
+    std::string Name;
+    int Hit_Points;
+    int Energy_Points;
+    int Attack_Damage;
+
+    
+};
+
+#endif
