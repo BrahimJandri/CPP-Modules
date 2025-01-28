46ac5a2d64dbf074df83f6323570bae804218c4c
BrahimJandri <bzaza066@gmail.com>
2025-01-28 13:04:22 +0100

    work work

--- cpp04/ex01/main.cpp
+++ cpp04/ex01/main.cpp
@@ -6,8 +6,8 @@
 
 int main()
 {
+    std::cout << "===========ANIMAL BASIC TEST=======" << std::endl;
     Animal *animals[arraySize];
-
     for (int i = 0; i < arraySize / 2; i++)
     {
         animals[i] = new Dog();
@@ -16,35 +16,29 @@ int main()
     {
         animals[i] = new Cat();
     }
-
-    std::cout << "===========MAKESOUND=======" << std::endl;
-
     for (int i = 0; i < arraySize; i++)
     {
         animals[i]->makeSound();
     }
-
-    std::cout << "===========DESTRUCTOR=======" << std::endl;
-
     for (int i = 0; i < arraySize; i++)
     {
         delete animals[i];
     }
-
-    std::cout << "===========DOG=======" << std::endl;
-
+    std::cout << "===========DOG SET IDEA BRAIN=======" << std::endl;
     Dog dog;
+    Dog dog2;
+    dog2 = dog;
     dog.getBrain()->setIdea("I am a dog", 0);
+    dog2.getBrain()->setIdea("I am a dog2", 0);
     std::cout << dog.getBrain()->getIdea(0) << std::endl;
-
-    std::cout << "===========CAT=======" << std::endl;
-
+    std::cout << dog2.getBrain()->getIdea(0) << std::endl;
+    std::cout << "===========CAT SET IDEA BRAIN=======" << std::endl;
     Cat cat;
+    Cat cat2(cat);
     cat.getBrain()->setIdea("I am a cat", 0);
+    cat2.getBrain()->setIdea("I am a cat2", 0);
     std::cout << cat.getBrain()->getIdea(0) << std::endl;
-    Dog j(dog);
-    j.getBrain()->setIdea("I am a j dog", 0);
-    std::cout << j.getBrain()->getIdea(0) << std::endl;
+    std::cout << cat2.getBrain()->getIdea(0) << std::endl;
     std::cout << "===========DESTRUCTOR=======" << std::endl;
 
     return 0;
