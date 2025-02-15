7981da9e2b0ff08722cc8f878211e26e99b0b0eb
BrahimJandri <bzaza066@gmail.com>
2025-02-15 13:54:30 +0100

    start the ex01

---
+++ cpp05/ex01/Bureaucrat.cpp
@@ -0,0 +1,92 @@
+#include "Bureaucrat.hpp"
+
+Bureaucrat::Bureaucrat() : _name("Bob"), _grade(1)
+{
+    std::cout << "Default Constractor Called" << std::endl;
+}
+
+Bureaucrat::Bureaucrat(const Bureaucrat &other) : _name(other._name), _grade(other._grade)
+{
+    if (this->_grade < 1)
+        throw GradeTooHighException();
+    else if (this->_grade > 150)
+        throw GradeTooLowException();
+    std::cout << "Copy Constructor Called" << std::endl;
+}
+
+
+Bureaucrat &Bureaucrat::operator=(const Bureaucrat &other)
+{
+    std::cout << "Copy Assignement Called" << std::endl;
+    if (this != &other)
+    {
+        this->_grade = other._grade;
+    }
+    return *this;
+}
+
+Bureaucrat::~Bureaucrat()
+{
+    std::cout << "Default Destractor Called" << std::endl;
+}
+
+Bureaucrat::Bureaucrat(const std::string &name, int grade)
+    : _name(name), _grade(grade)
+{
+    if (_grade < 1)
+        throw GradeTooHighException();
+    if (_grade > 150)
+        throw GradeTooLowException();
+}
+
+const std::string &Bureaucrat::getName() const
+{
+    return this->_name;
+}
+
+int Bureaucrat::getGrade() const
+{
+    return this->_grade;
+}
+
+void Bureaucrat::incrementGrade()
+{
+    _grade--;
+    if (_grade < 1)
+    {
+        throw GradeTooHighException();
+    }
+    if (_grade > 150)
+    {
+        throw GradeTooLowException();
+    }
+}
+
+void Bureaucrat::decrementGrade()
+{
+    _grade++;
+    if (_grade < 1)
+    {
+        throw GradeTooHighException();
+    }
+    if (_grade > 150)
+    {
+        throw GradeTooLowException();
+    }
+}
+
+std::ostream &operator<<(std::ostream &out, const Bureaucrat &b)
+{
+    out << b.getName() << ", bureaucrat grade " << b.getGrade();
+    return out;
+}
+
+const char *Bureaucrat::GradeTooHighException::what() const throw()
+{
+    return "Grade Too High Exception";
+}
+
+const char *Bureaucrat::GradeTooLowException::what() const throw()
+{
+    return "Grade Too Low Exception";
+}
