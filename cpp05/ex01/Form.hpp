bc458a0f080b65b7654b53b0a2b9c4767a953c24
BrahimJandri <bzaza066@gmail.com>
2025-02-18 10:53:44 +0100

    work on header form

--- cpp05/ex01/Form.hpp
+++ cpp05/ex01/Form.hpp
@@ -1,12 +1,20 @@
 #ifndef FORM_HPP
 #define FORM_HPP
 
+#include <iostream>
+#include <string>
+#include <stdexcept>
+
+
 class Form
 {
 private:
-    /* data */
+    const std::string _name;
+    bool _signed;
+    const int _gradeSigne;
+    const int _gradeExec;
 public:
-    Form(/* args */);
+    Form();
     ~Form();
 };
 
