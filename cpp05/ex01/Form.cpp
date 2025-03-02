972717f888b78651a2e12c67780ec8c95da8f43f
BrahimJandri <bzaza066@gmail.com>
2025-03-02 15:26:35 +0000

    work on form

--- cpp05/ex01/Form.cpp
+++ cpp05/ex01/Form.cpp
@@ -1,36 +1,88 @@
+// Form.cpp
 #include "Form.hpp"
 
-Form::Form() : _signed(false), _name(""), _gradeExec(10), _gradeSigne(10)
+// Constructors and Destructor
+Form::Form() : _name("Default Form"), _signed(false), _gradeToSign(150), _gradeToExecute(150)
 {
-    std::cout << "Default Constructer Called" << std::endl;
+}
+
+Form::Form(const std::string &name, int gradeToSign, int gradeToExecute)
+    : _name(name), _signed(false), _gradeToSign(gradeToSign), _gradeToExecute(gradeToExecute)
+{
+    if (gradeToSign < 1 || gradeToExecute < 1)
+        throw Form::GradeTooHighException();
+    if (gradeToSign > 150 || gradeToExecute > 150)
+        throw Form::GradeTooLowException();
+}
+
+Form::Form(const Form &other)
+    : _name(other._name), _signed(other._signed),
+      _gradeToSign(other._gradeToSign), _gradeToExecute(other._gradeToExecute)
+{
 }
 
 Form::~Form()
 {
-    std::cout << "Default Destructer Called" << std::endl;
 }
 
+// Assignment operator
+Form &Form::operator=(const Form &other)
+{
+    if (this != &other)
+    {
+        // Can't assign to const attributes, but we can copy the signed status
+        _signed = other._signed;
+    }
+    return *this;
+}
+
+// Getters
+std::string Form::getName() const
+{
+    return _name;
+}
+
+bool Form::isSigned() const
+{
+    return _signed;
+}
+
+int Form::getGradeToSign() const
+{
+    return _gradeToSign;
+}
+
+int Form::getGradeToExecute() const
+{
+    return _gradeToExecute;
+}
+
+// Member functions
+void Form::beSigned(const Bureaucrat &bureaucrat)
+{
+    if (bureaucrat.getGrade() <= _gradeToSign)
+        _signed = true;
+    else
+        throw Form::GradeTooLowException();
+}
+
+// Exception classes
 const char *Form::GradeTooHighException::what() const throw()
 {
-    return "Grade Too High Exception";
+    return "Form grade is too high";
 }
 
 const char *Form::GradeTooLowException::what() const throw()
 {
-    return "Grade Too Low Exception";
+    return "Form grade is too low";
 }
 
-std::ostream &operator<<(std::ostream &out, const Form &b)
+// Insertion operator overload
+std::ostream &operator<<(std::ostream &os, const Form &form)
 {
-    return out;
-}
-
-int Form::getGradeSigne()
-{
-    return _gradeSigne;
-}
-
-int Form::getGradeExec()
-{
-    return _gradeExec;
+    os << "Form: " << form.getName()
+       << ", signed: " << (form.isSigned() ? "yes" : "no")
+       << ", grade to sign: " << form.getGradeToSign()
+       << ", grade to execute: " << form.getGradeToExecute();
+    return os;
 }
--- cpp05/ex01/Form.hpp
+++ cpp05/ex01/Form.hpp
@@ -6,38 +6,47 @@
 #include "Bureaucrat.hpp"
 
 
-class Form
-{
+class Bureaucrat; // Forward declaration to avoid circular dependency
+
+class Form {
 private:
     const std::string _name;
     bool _signed;
-    const int _gradeSigne;
-    const int _gradeExec;
+    const int _gradeToSign;
+    const int _gradeToExecute;
+
 public:
+    // Constructors and Destructor
     Form();
+    Form(const std::string& name, int gradeToSign, int gradeToExecute);
+    Form(const Form& other);
     ~Form();
-    beSigned(Bureaucrat &Bureaucrat);
 
-    class GradeTooHighException : public std::exception
-    {
+    // Assignment operator
+    Form& operator=(const Form& other);
+
+    // Getters
+    std::string getName() const;
+    bool isSigned() const;
+    int getGradeToSign() const;
+    int getGradeToExecute() const;
+
+    // Member functions
+    void beSigned(const Bureaucrat& bureaucrat);
+
+    // Exception classes
+    class GradeTooHighException : public std::exception {
     public:
-        const char *what() const throw();
+        virtual const char* what() const throw();
     };
 
-    class GradeTooLowException : public std::exception
-    {
+    class GradeTooLowException : public std::exception {
     public:
-        const char *what() const throw();
+        virtual const char* what() const throw();
     };
-
-    int getGradeSigne();
-    int getGradeExec();
-    const std::string &getName() const;
-    bool getBoolSign();
-
 };
 
-std::ostream &operator<<(std::ostream &out, const Form &b);
-
+// Insertion operator overload
+std::ostream& operator<<(std::ostream& os, const Form& form);
 
 #endif
