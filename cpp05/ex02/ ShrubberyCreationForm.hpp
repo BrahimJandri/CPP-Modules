6f5af9c80bc655cac94bfa1399a9f4e77abd68e2
BrahimJandri <bzaza066@gmail.com>
2025-04-09 11:43:57 +0100

    start ex02

---
+++ cpp05/ex02/ ShrubberyCreationForm.hpp
@@ -0,0 +1,15 @@
+#ifndef SHRUBBERYCREATIONFORM_HPP
+#define SHRUBBERYCREATIONFORM_HPP
+
+#include "Aform.hpp"
+
+class ShrubberyCreationForm : public Form
+{
+private:
+    
+public:
+    ShrubberyCreationForm();
+    ~ShrubberyCreationForm();
+};
+
+#endif
