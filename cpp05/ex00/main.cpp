b8a37a141b922f3f27e90537de15af34478ec464
BrahimJandri <bzaza066@gmail.com>
2025-04-11 11:53:18 +0100

    fix  ex01 understand more about try catch

--- cpp05/ex00/main.cpp
+++ cpp05/ex00/main.cpp
@@ -4,20 +4,17 @@ int main()
 {
     try
     {
-        Bureaucrat obj("John", 150);
-        std::cout << obj << std::endl;
-        std::cout << "------------------------" << std::endl;
-        obj.incrementGrade();
-        std::cout << obj << std::endl;
-        std::cout << "------------------------" << std::endl;
-        Bureaucrat obj2;
-        obj2 = obj;
-        obj2.decrementGrade();
-        std::cout << obj2 << std::endl;
-        std::cout << "------------------------" << std::endl;
-        Bureaucrat obj3(obj2);
-        std::cout << obj3 << std::endl;
-        obj3.decrementGrade();
+        Bureaucrat John("John", 150);
+        std::cout << John << std::endl;
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << std::endl;
+    }
+    try
+    {
+        Bureaucrat Bob;
+        std::cout << Bob << std::endl;
     }
     catch(const std::exception& e)
     {
