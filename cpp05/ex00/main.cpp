6cf5c2483aef5e5f4cdcf5fdd4baea39115eb020
BrahimJandri <bzaza066@gmail.com>
2025-04-12 18:18:08 +0100

    add test to ex00

--- cpp05/ex00/main.cpp
+++ cpp05/ex00/main.cpp
@@ -2,23 +2,53 @@
 
 int main()
 {
+    Bureaucrat John("John", 150);
+    Bureaucrat Alice;
+    Bureaucrat Bob(Alice);
+
     try
     {
-        Bureaucrat John("John", 150);
         std::cout << John << std::endl;
     }
-    catch(const std::exception& e)
+    catch (const std::exception &e)
     {
         std::cerr << e.what() << std::endl;
     }
     try
     {
-        Bureaucrat Bob;
-        std::cout << Bob << std::endl;
+        John.decrementGrade();
     }
-    catch(const std::exception& e)
+    catch (const std::exception &e)
     {
         std::cerr << e.what() << std::endl;
     }
+    try
+    {
+        John.incrementGrade();
+        std::cout << John << std::endl;
+    }
+    catch (const std::exception &e)
+    {
+        std::cerr << e.what() << std::endl;
+    }
+    try
+    {
+        Alice = John;
+        Alice.decrementGrade();
+        std::cout << Alice << std::endl;
+    }
+    catch (const std::exception &e)
+    {
+        std::cerr << e.what() << std::endl;
+    }
+    try
+    {
+        std::cout << Bob << std::endl;
+    }
+    catch (const std::exception &e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+
     return 0;
 }
