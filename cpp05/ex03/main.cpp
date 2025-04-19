e276b073807ef6e916f23bd3ac0c57dfbb228328
BrahimJandri <bzaza066@gmail.com>
2025-04-19 09:06:25 +0100

    test some cases

--- cpp05/ex03/main.cpp
+++ cpp05/ex03/main.cpp
@@ -2,16 +2,35 @@
 
 int main()
 {
-    Intern someRandomIntern;
-    AForm *form;
+    try
+    {
+        Intern someRandomIntern;
+        AForm *form;
 
-    form = someRandomIntern.makeForm("robotomy rEquest", "Bender");
-    if (form)
-        delete form;
+        form = someRandomIntern.makeForm("robotomy request", "Bender");
+        if (form)
+        {
+            std::cout << *form << std::endl;
+            delete form;
+        }
 
-    form = someRandomIntern.makeForm("Shrubbery creation", "Nobody");
-    if (form)
-        delete form;
+        form = someRandomIntern.makeForm("shrubbery CREATION", "Home");
+        if (form)
+        {
+            std::cout << *form << std::endl;
+            delete form;
+        }
+
+        form = someRandomIntern.makeForm("invalid form", "nowhere");
+        if (form)
+        {
+            delete form;
+        }
+    }
+    catch (std::exception &e)
+    {
+        std::cerr << e.what() << std::endl;
+    }
 
     return 0;
 }
