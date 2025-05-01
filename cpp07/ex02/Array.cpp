0963fc51d028d5b4eb28fe72dcedc9b28fe1b46e
BrahimJandri <bzaza066@gmail.com>
2025-05-01 11:58:30 +0100

    fix some issue

--- cpp07/ex02/Array.cpp
+++
@@ -1,8 +0,0 @@
-#include <Array.hpp>
-
-Array::Array()
-{
-    int s = 10;
-    this._size = s;
-    
-}
--- cpp07/ex02/Array.hpp
+++ cpp07/ex02/Array.hpp
@@ -1,15 +1,63 @@
-#pragma once
+#ifndef ARRAY_HPP
+#define ARRAY_HPP
 
 #include <iostream>
+#include <stdexcept>
+#include <cstdlib>
 
 template <typename T>
 class Array
 {
 private:
-    int _size;
-    T *myArray;
+    T *data;
+    unsigned int _size;
 
 public:
-    Array();
-    ~Array();
+    Array() : data(NULL), _size(0) {}
+
+    Array(unsigned int n) : data(new T[n]()), _size(n) {}
+
+    Array(const Array &other) : data(NULL), _size(0)
+    {
+        *this = other;
+    }
+
+    Array &operator=(const Array &other)
+    {
+        if (this != &other)
+        {
+            delete[] data;
+            _size = other._size;
+            data = new T[_size];
+            for (unsigned int i = 0; i < _size; ++i)
+                data[i] = other.data[i];
+        }
+        return *this;
+    }
+
+    ~Array()
+    {
+        delete[] data;
+    }
+
+    T &operator[](unsigned int index)
+    {
+        if (index >= _size)
+            throw std::out_of_range("Index out of bounds");
+        return data[index];
+    }
+
+    const T &operator[](unsigned int index) const
+    {
+        if (index >= _size)
+            throw std::out_of_range("Index out of bounds");
+        return data[index];
+    }
+
+    unsigned int size() const
+    {
+        return _size;
+    }
 };
+
+#endif
