53bc253dc2473dc604788e633c70b1dc919bbe72
BrahimJandri <bzaza066@gmail.com>
2025-05-05 11:46:31 +0100

    first ex done

--- cpp08/ex00/easyfind.hpp
+++ cpp08/ex00/easyfind.hpp
@@ -1,9 +1,18 @@
 #pragma once
 
 #include <iostream>
+#include <vector>
+#include <stdexcept>
 
 template <typename T>
-void easyfind(T &data, size_t a)
+int easyfind(T &data, int target)
 {
-    
+    for (size_t i = 0; i < data.size(); ++i)
+    {
+        if (data[i] == target)
+        {
+            return static_cast<int>(i);
+        }
+    }
+    throw std::runtime_error("Value not found in vector.");
 }
--- cpp08/ex00/main.cpp
+++ cpp08/ex00/main.cpp
@@ -2,5 +2,22 @@
 
 int main()
 {
-    
+    std::vector<int> vec;
+    vec.push_back(25);
+    vec.push_back(2);
+    vec.push_back(5);
+    vec.push_back(45);
+    vec.push_back(50);
+    vec.push_back(58);
+    int target = 405;
+    try
+    {
+        int index = easyfind(vec, target);
+        std::cout << "Found " << target << " at index: " << index << std::endl;
+    }
+    catch (const std::runtime_error &e)
+    {
+        std::cerr << "Error: " << e.what() << std::endl;
+    }
+    return 0;
 }
